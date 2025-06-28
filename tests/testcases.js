d283ab2fcb40dcf367dd30ed506d4853d323f661
2410781005 <2410781005@fh-burgenland.at>
2025-06-28 12:13:23 +0200

    Update testcases.js

--- tests/testcases.js
+++ tests/testcases.js
@@ -1,6 +1,6 @@
 export const testCases = [
     {
-      name: "TC1 – StraightThrough",
+      name: "TC1 – StraightThrough - TEST",
       make: "Ford",
       enginePerformance: 500,
       dateOfManufactor: "dateMinus3years",
